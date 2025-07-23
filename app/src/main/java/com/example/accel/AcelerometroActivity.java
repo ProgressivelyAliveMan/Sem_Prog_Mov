4e4b0b9d546baa1b13a5657e18ebc11470eb5505
rodrigo molinar <juanruizo777@gmail.com>
2025-07-22 19:31:28 -0500

    Actualizar código para nombre de la clase

--- app/src/main/java/com/example/accel/AcelerometroActivity.java
+++ app/src/main/java/com/example/accel/AcelerometroActivity.java
@@ -15,18 +15,18 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class AccelerometerActivity extends AppCompatActivity implements SensorEventListener {
+public class AcelerometroActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor acelerometro;
     private TextView acelerometroDatos;
     private Button btnVolver;
-    private ConstraintLayout mainLayout; // Variable para el layout principal
+    private ConstraintLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accelerometer);
+        setContentView(R.layout.activity_acelerometro);
 
         // Inicializar vistas
         acelerometroDatos = findViewById(R.id.acelerometro_datos);
